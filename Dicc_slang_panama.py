4051d99456c502b2a3200cd6ed57128f7161612c
Alberthny <m.alberto.m.19@gmail.com>
2023-08-20 20:30:51 -0500

    one commit moreee

--- Dicc_slang_panama.py
+++ Dicc_slang_panama.py
@@ -1,119 +1,88 @@
 import sqlite3
 
-def crear_tabla():
-    # Conexión a la base de datos o creación si no existe
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
-    
-    # Crear tabla si no existe
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS palabras (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            palabra TEXT NOT NULL UNIQUE,
-            significado TEXT NOT NULL
-        )
-    """)
-    
-    conn.commit()
-    conn.close()
+# Conectar a la base de datos (creará el archivo si no existe)
+conn = sqlite3.connect('slang_panameno.db')
+cursor = conn.cursor()
 
+# Crear la tabla para almacenar las palabras
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS palabras (
+        id INTEGER PRIMARY KEY,
+        palabra TEXT UNIQUE,
+        significado TEXT
+    )
+''')
+
+conn.commit()
 def agregar_palabra():
-    palabra = input("Ingrese la nueva palabra: ")
-    significado = input("Ingrese el significado de la palabra: ")
-
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
+    palabra = input("Ingresa la palabra: ")
+    significado = input("Ingresa el significado: ")
     
-    # Insertar nueva palabra en la tabla
-    cursor.execute("INSERT INTO palabras (palabra, significado) VALUES (?, ?)", (palabra, significado))
-    
-    conn.commit()
-    conn.close()
+    try:
+        cursor.execute("INSERT INTO palabras (palabra, significado) VALUES (?, ?)", (palabra, significado))
+        conn.commit()
+        print("Palabra agregada exitosamente.")
+    except sqlite3.IntegrityError:
+        print("La palabra ya existe en el diccionario.")
 
 def editar_palabra():
-    palabra = input("Ingrese la palabra que desea editar: ")
-    nuevo_significado = input("Ingrese el nuevo significado de la palabra: ")
-
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
+    palabra = input("Ingresa la palabra a editar: ")
+    nuevo_significado = input("Ingresa el nuevo significado: ")
     
-    # Actualiza
     cursor.execute("UPDATE palabras SET significado = ? WHERE palabra = ?", (nuevo_significado, palabra))
-    
     conn.commit()
-    conn.close()
+    print("Significado actualizado.")
 
 def eliminar_palabra():
-    palabra = input("Ingrese la palabra que desea eliminar: ")
-
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
+    palabra = input("Ingresa la palabra a eliminar: ")
     
-    # Eliminar la palabra de la tabl
     cursor.execute("DELETE FROM palabras WHERE palabra = ?", (palabra,))
-    
     conn.commit()
-    conn.close()
+    print("Palabra eliminada.")
 
 def ver_listado_palabras():
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
-    
-    # Obtener todas las palabras y sus significados
     cursor.execute("SELECT palabra, significado FROM palabras")
     palabras = cursor.fetchall()
-
+    
     for palabra, significado in palabras:
         print(f"{palabra}: {significado}")
-    
-    conn.close()
 
 def buscar_significado():
-    palabra = input("Ingrese la palabra que desea buscar: ")
-
-    conn = sqlite3.connect("slang_panameno.db")
-    cursor = conn.cursor()
+    palabra = input("Ingresa la palabra a buscar: ")
     
-    # Buscar la palabra en la tabla
     cursor.execute("SELECT significado FROM palabras WHERE palabra = ?", (palabra,))
     resultado = cursor.fetchone()
-
-    if resultado:
-        print(f"{palabra}: {resultado[0]}")
-    else:
-        print(f"La palabra '{palabra}' no fue encontrada en el diccionario.")
     
-    conn.close()
+    if resultado:
+        print(f"Significado: {resultado[0]}")
+    else:
+        print("La palabra no fue encontrada en el diccionario.")
 
-def main():
-    crear_tabla()
+# Menú principal
+while True:
+    print("\nMenú:")
+    print("a) Agregar nueva palabra")
+    print("c) Editar palabra existente")
+    print("d) Eliminar palabra existente")
+    print("e) Ver listado de palabras")
+    print("f) Buscar significado de palabra")
+    print("g) Salir")
 
-    while True:
-        print("\n--- Diccionario de Slang Panameño ---")
-        print("a) Agregar nueva palabra")
-        print("b) Editar palabra existente")
-        print("c) Eliminar palabra existente")
-        print("d) Ver listado de palabras")
-        print("e) Buscar significado de palabra")
-        print("f) Salir")
+    opcion = input("Elige una opción: ")
 
-        opcion = input("Seleccione una opción: ").lower()
-
-        if opcion == "a":
-            agregar_palabra()
-        elif opcion == "b":
-            editar_palabra()
-        elif opcion == "c":
-            eliminar_palabra()
-        elif opcion == "d":
-            ver_listado_palabras()
-        elif opcion == "e":
-            buscar_significado()
-        elif opcion == "f":
-            print("¡Hasta luego!")
-            break
-        else:
-            print("Opción inválida. Intente nuevamente.")
-
-if __name__ == "__main__":
-    main()
+    if opcion == 'a':
+        agregar_palabra()
+    elif opcion == 'c':
+        editar_palabra()
+    elif opcion == 'd':
+        eliminar_palabra()
+    elif opcion == 'e':
+        ver_listado_palabras()
+    elif opcion == 'f':
+        buscar_significado()
+    elif opcion == 'g':
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción inválida. Por favor, elige una opción válida.")
+conn.close()
